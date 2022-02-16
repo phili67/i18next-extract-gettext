70b6c14877bee4affea3c08d8c4c43e2b761a922
EcclesiaCRM <philippe.logel@gmail.com>
2022-02-16 22:09:53 +0100

    Update index.js
    Bug in some cases

--- index.js
+++ index.js
@@ -10,54 +10,75 @@ var fileGlob = args.files
 var outputFile = args.output
 var namespace = args.ns
 
-if (namespace == undefined) {
-    namespace = "translation"
-}
+var parserOptions = {};
 
-var parserOptions = {
-    debug: true,
+if (namespace == undefined) {
+    namespace = "translation";
+    
+    parserOptions = {
+    // Include react helpers into parsing
+    attr: {
+      list: ['data-i18n', 'i18nKey']
+    },
     func: {
-        list: ['i18next.t', 'i18n.t'],
-        extensions: ['.js', '.jsx']
+      list: ['i18next.t', 'i18n.t', 't']
     },
-    trans: {
-        component: 'Trans',
-        i18nKey: 'i18nKey',
-        defaultsKey: 'defaults',
-        extensions: ['.js', '.jsx'],
-        fallbackKey: function(ns, value) {
-            return value;
-        },
-        acorn: {
-            ecmaVersion: 2020,
-            sourceType: 'module', // defaults to 'module'
-            // Check out https://github.com/acornjs/acorn/tree/master/acorn#interface for additional options
-        }
-    },
-    lngs: ['en'],
-    ns: [
-        'locale',
-        namespace
-    ],
-    defaultLng: 'en',
-    defaultNs: 'translation',
-    defaultValue: '',
-    resource: {
-        loadPath: 'i18n/{{lng}}/{{ns}}.json',
-        savePath: 'i18n/{{lng}}/{{ns}}.json',
-        jsonIndent: 2,
-        lineEnding: '\n'
-    },
-    nsSeparator: true, // namespace separator
-    keySeparator: false, // key separator
+    // Make sure common separators don t break the string
+    keySeparator: args.keySeparator || '°°°°°°.°°°°°°',
+    nsSeparator: args.nsSeparator || '°°°°°°:°°°°°°',
+    pluralSeparator: args.pluralSeparator || '°°°°°°_°°°°°°',
+    contextSeparator: args.contextSeparator || '°°°°°°_°°°°°°',
+    // Interpolate correctly
     interpolation: {
-        prefix: '{{',
-        suffix: '}}'
+      prefix: '{{',
+      suffix: '}}'
     }
+  }
+} else {
+  parserOptions = {
+      debug: true,
+      func: {
+          list: ['i18next.t', 'i18n.t'],
+          extensions: ['.js', '.jsx']
+      },
+      trans: {
+          component: 'Trans',
+          i18nKey: 'i18nKey',
+          defaultsKey: 'defaults',
+          extensions: ['.js', '.jsx'],
+          fallbackKey: function(ns, value) {
+              return value;
+          },
+          acorn: {
+              ecmaVersion: 2020,
+              sourceType: 'module', // defaults to 'module'
+              // Check out https://github.com/acornjs/acorn/tree/master/acorn#interface for additional options
+          }
+      },
+      lngs: ['en'],
+      ns: [
+          'locale',
+          namespace
+      ],
+      defaultLng: 'en',
+      defaultNs: 'translation',
+      defaultValue: '',
+      resource: {
+          loadPath: 'i18n/{{lng}}/{{ns}}.json',
+          savePath: 'i18n/{{lng}}/{{ns}}.json',
+          jsonIndent: 2,
+          lineEnding: '\n'
+      },
+      nsSeparator: true, // namespace separator
+      keySeparator: false, // key separator
+      interpolation: {
+          prefix: '{{',
+          suffix: '}}'
+      }
+  }
 }
 
 var parser = new Parser(parserOptions)
-
 
 if (!fileGlob || !outputFile) {
     console.log('Missing "files" glob or "output" file')
